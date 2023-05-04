22e2d9670ff73f82c7f8ced3fd5a63c30b6b2bd9
John Kipruto <johnkipruto157@gmail.com>
2023-05-04 10:56:46 +0300

    Add netlify functions

---
+++ src/netlify/functions/bracelets.js
@@ -0,0 +1,80 @@
+let bracelets = [
+    {
+        "id" : 1,
+        "description" : "4-12mm Round ABS Cheap Shape Imitation Pearls White Beads Handmade DIY Bracelet Jewelry Accessories Making Wholesale",
+        "image" : "https://wallpapercave.com/wp/wp2487338.jpg",
+        "amount" : 1500,
+        "rating" : 7
+    },
+    {
+        "id" : 2,
+        "description" : "15g/lot 2mm 3mm 4mm Czech Glass Solid Seed Beads DIY Bracelet Beads For Jewelry Making Accessories",
+        "image" : "https://www.vhv.rs/dpng/d/146-1466197_22ct-mens-gold-bracelet-bracelet-hd-png-download.png",
+        "amount" : 1500,
+        "rating": 4
+    },
+    {
+        "id" : 3,
+        "description" : "Trend Sunflower Flower Resin Earrings for Women Korean Fashion Funny Designer Earrings Elegant Unique Geometric Earring Fun",
+        "image" : "https://e1.pxfuel.com/desktop-wallpaper/794/137/desktop-wallpaper-bracelet-gold-jewelry-thumbnail.jpg",
+        "amount" : 1500,
+        "rating" : 10
+    },
+    {
+        "id" : 4,
+        "description" : "Isywaka Light Blue Colors 4*6mm 50pcs Rondelle Austria faceted Crystal Glass Beads Loose Spacer Round Beads for Jewelry Making",
+        "image" : "https://m.media-amazon.com/images/I/61pLMB8VUZL._AC_SX522_.jpg",
+        "amount" : 1500,
+        "rating" : 8
+    },
+    {
+        "id" : 5,
+        "description" : "Brand new original European charm puppy, beautiful rainbow suitable for Pandora bracelet DIY women's jewelry",
+        "image" : "https://wallpaperaccess.com/full/2135566.jpg",
+        "amount" : 1500,
+        "rating" : 8
+    },
+    {
+        "id" : 6,
+        "description" : "Astronaut Enamel Pin Adventure Ocean Drifting Wishing Bottle Brooches Bag Lapel Pin Badge Artistic Design Brooch Pin Wholesale",
+        "image" : "https://images.unsplash.com/photo-1619119069152-a2b331eb392a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJhY2VsZXR8ZW58MHx8MHx8&w=1000&q=80"
+        ,"amount" : 1500,
+        "rating" : 6
+    },
+    {
+        "id" : 7,
+        "description" : "Approx.1000pcs 2mm Cream Color Czech Glass Beads for Jewelry Making DIY Beads Round Bracelet Necklace Earrings Accessories",
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4zJo7XbdSGAWJLNQHCjx1Ji0lYu17Oa3MgA&usqp=CAU"
+        ,"amount" : 1500,
+        "rating" : 5
+    },
+    {
+        "id" : 8,
+        "description" : "Cat Earrings Luna and Artemis Anime Inspired Enamel Drop Earrings Kawaii Animal Jewelry for Women Girl Gift",
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5f9qaO68HcHAVVtACb7QaiXHoCUQ36GOZltYyu5ucPuchW_vg0M2NtNT-TjQ6rafG38w&usqp=CAU"
+        ,"amount" : 1500,
+        "rating": 10
+    },
+    {
+        "id" : 9,
+        "description" : "Isywaka White Multicolor 4*6mm 50pcs Rondelle Austria faceted Crystal Glass Beads Loose Spacer Round Beads for Jewelry Making",
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAt5s2iigrft7UhN3DbaUQ-ZRJEE7AXIwcuA&usqp=CAU",
+        "amount" : 1500,
+        "rating" : 3
+    },
+    {
+        "id" : 10,
+        "description" : "Wholesale colorful multicolor 2mm 3mm 4mm Czech glass beads for bracelet necklace earrings jewelry DIY seed beads materia",
+        "image" : "https://cdn.shopify.com/s/files/1/0003/6500/2803/products/B496-Diamond-20k-Rose-Black-1_600x.jpg?v=1665023329"
+        ,"amount" : 1500,
+        "rating" : 5
+    }
+
+]
+
+exports.handler = async () => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(bracelets),
+    };
+  };
---
+++ src/netlify/functions/handbags.js
@@ -0,0 +1,79 @@
+let handbags = [
+    {
+        "id" : 1,
+        "description" : "Designer Handbag Women Shoulder Bag Ladies Handbag Women Purse",
+        "image" : "https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZGJhZ3xlbnwwfHwwfHw%3D&w=1000&q=80"
+        ,"amount" : 1500,
+        "rating" : 9
+    },
+    {
+        "id" : 2,
+        "description" : "(WDL7429) Business Handbag Ladies Side Bag Branded Purse for Women Womens Designer Handbags",
+        "image" : "https://m.media-amazon.com/images/I/51erAzSXnwL._UY535_.jpg",
+        "amount" : 1500,
+        "rating" :8
+    },
+    {
+        "id" : 3,
+        "description" : "Small Makeup Bag Handy Travel Cosmetic Toiletry Pouch Leopard Snakeskin Handbag Leather Purse for Women Girls",
+        "image" : "https://m.media-amazon.com/images/I/71hFps3M3lL._UX500_.jpg",
+        "amount" : 1500,
+        "rating": 9
+    },
+    {
+        "id" : 4,
+        "description" : "New Arrival Crocodile Pattern Leather Ladies Handbag Women Purse",
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5dqHcNXJ0-W9NAVOrtMLhAIwFxWk78wFOKQ&usqp=CAU",
+        "amount" : 1500, 
+        "rating" : 9
+    },
+    {
+        "id" : 5,
+        "description" : "New Arrival Crocodile Pattern Leather Ladies Handbag Women Purse",
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWjoTuuL8LOYt3WbYPR1IA3YpostqmYbihKYaKtMdpfHH5cten4brSTfO3Er2Lp4YbEI4&usqp=CAU"
+        ,"amount" : 1500, 
+        "rating" : 7
+    },
+    {
+        "id" : 6,
+        "description" : "High Quality Popular Daily Use Outdoor Shopping Women Handbags PU Leather Lady Shoulder Purses",
+        "image" : "https://www.chanel.com/images/t_one////q_auto:good,f_auto,fl_lossy,dpr_1.2/w_1920//mini-flap-bag-light-green-white-ecru-shaded-sequins-gold-tone-metal-shaded-sequins-gold-tone-metal-packshot-artistique-vue1-as3965b10633no020-9521332781086.jpg"
+        ,"amount" : 1500, 
+        "rating" : 7
+    },
+    {
+        "id" : 7,
+        "description" : "Wholesale Custom Female Shoulder Bag Crossbody Purse for Lady Fashion PU Leather Women Handbag",
+        "image" : "https://www.chanel.com/images/t_one///q_auto:good,f_auto,fl_lossy,dpr_1.2/w_1920//mini-evening-bag-light-pink-wool-tweed-gold-tone-metal-wool-tweed-gold-tone-metal-packshot-default-as3526b09388nl020-8857327435806.jpg"
+        ,"amount" : 1500,
+        "rating" : 8
+    },
+    {
+        "id" : 8,
+        "description" : "Factory a Wholesale Exquisite Leather Women′s Bags Fashion Bag Crossbody Bag Single Shoulder Handbag Purse",
+        "image" : "https://a.1stdibscdn.com/chanel-yellow-tweed-medium-19-flap-bag-for-sale/v_13101/1678097685090/luxury_women_chanel_used_handbags_p757669_014_master.jpg?width=768"
+        ,"amount" : 1500, 
+        "rating" : 8
+    },
+    {
+        "id" : 9,
+        "description" : "Wholesale Replicas Bags Handbags Designer Bag Handbags Women Purses",
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0WvfS_AGfI7ht2bdIRkLu9OzSwOJk2TfHfA&usqp=CAU",
+        "amount" : 1500,
+        "rating" : 8
+    },
+    {
+        "id" : 10,
+        "description" : "Top-Design Cowhide Leather Polka-DOT Wallet Fashion Classical Woman Purses",
+        "image" : "https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/24/4000x2250/hd-aspect-1466178782-gettyimages-540778074.jpg?resize=1200:*"
+        ,"amount" : 1500,
+        "rating" : 8
+    }
+]
+
+exports.handler = async () => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(handbags),
+    };
+  };
